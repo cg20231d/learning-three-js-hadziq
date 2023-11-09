4067955d721485bfa2effb7c95e8d4a3fe5b424f
Hadziq Fabroyir <hadziq@gmail.com>
2023-11-09 07:35:09 +0700

    Create a sphere

--- app.js
+++ app.js
@@ -22,40 +22,40 @@ const albedoTexture = textureLoader.load("albedo.jpg");
 const normalsTexture = textureLoader.load("normals.jpg");
 const roughnessTexture = textureLoader.load("roughness.jpg");
 
-// Create the cube geometry (vertex shader)
-const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+// Create the object geometry (vertex shader)
+const objectGeometry = new THREE.SphereGeometry(2, 16, 32);
 
-// Create the cube material (fragment shader)
-const cubeMaterial = new THREE.MeshPhongMaterial({
+// Create the object material (fragment shader)
+const objectMaterial = new THREE.MeshPhongMaterial({
     //color: 0xff00ff // This sets the color to red. You can change the hex value to any color you prefer.
     map: albedoTexture,
     //displacementMap: displacementTexture, // Displacement map
     normalMap: normalsTexture, // Normal map
     roughnessMap: roughnessTexture // Roughness map
 });
-//cubeMaterial.displacementScale = 0.2; // Adjust the value as needed
-cubeMaterial.normalScale.set(0.5, 0.5); // Adjust the values as needed
+//objectMaterial.displacementScale = 0.2; // Adjust the value as needed
+objectMaterial.normalScale.set(0.5, 0.5); // Adjust the values as needed
 
-// Create the cube as a 3D object a.k.a. mesh
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube);
+// Create the object as a 3D object a.k.a. mesh
+const object = new THREE.Mesh(objectGeometry, objectMaterial);
+scene.add(object);
 
 // Create a plane geometry
 const planeGeometry = new THREE.PlaneGeometry(5, 5); // Adjust the size as needed
 
 // Create a green material for the plane
 const planeMaterial = new THREE.MeshPhongMaterial({
-    color: 0x00ff00 // Green color
-    //map: albedoTexture,
+    //color: 0x00ff00, // Green color
+    map: albedoTexture,
     //displacementMap: displacementTexture, // Displacement map
-    //normalMap: normalsTexture, // Normal map
-    //roughnessMap: roughnessTexture // Roughness map
+    normalMap: normalsTexture, // Normal map
+    roughnessMap: roughnessTexture // Roughness map
 });
 
 // Create the plane as a 3D object (mesh)
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2; // Rotate the plane to be horizontal
-plane.position.y = -2; // Adjust the position so it's below the cube
+plane.position.y = -2; // Adjust the position so it's below the object
 scene.add(plane);
 
 // Set up the ambient light
@@ -88,13 +88,13 @@ function onDocumentKeyDown(event) {
     var keyCode = event.which;
 
     if (keyCode == 37) {         // Left Arrow
-        cube.position.x -= 0.1; // Adjust the position or speed as needed
+        object.position.x -= 0.1; // Adjust the position or speed as needed
     } else if (keyCode == 39) {  // Right Arrow
-        cube.position.x += 0.1;
+        object.position.x += 0.1;
     } else if (keyCode == 38) {  // Up Arrow
-        cube.position.y += 0.1;
+        object.position.y += 0.1;
     } else if (keyCode == 40) {  // Down Arrow
-        cube.position.y -= 0.1;
+        object.position.y -= 0.1;
     }
 }
 document.addEventListener('keydown', onDocumentKeyDown, false);
@@ -102,10 +102,10 @@ document.addEventListener('keydown', onDocumentKeyDown, false);
 
 // Render loop
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
-    camera.lookAt(cube.position);
+    object.rotation.x += 0.01;
+    object.rotation.y += 0.01;
+    object.rotation.z += 0.01;
+    camera.lookAt(object.position);
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
